b415a9984128e7bba46254456e4fc53eeed129ac
richi <richidhimar45@gmail.com>
2022-11-12 15:33:37 +0530

    added the fields

--- main.py
+++ main.py
@@ -1,15 +1,30 @@
 from typing import Union
-
+from typing import Optional
 from fastapi import FastAPI
+from pydantic import BaseModel
+
+# app = FastAPI()
+
+
+# @app.get("/")
+# def read_root():
+#     return {"Hello": "World"}
+
+
+# @app.get("/items/{item_id}")
+# def read_item(item_id: int, q: Union[str, None] = None):
+#     return {"item_id": item_id, "q": q}
+
+
+# ------------------------------------
+class Item(BaseModel):
+    name: str
+    description: Optional[str] = None
+    price: float
+    tax: Optional[float] = None
 
 app = FastAPI()
 
-
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
-
-
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
+@app.post("/items/")
+async def create_item(item: Item):
+    return item
